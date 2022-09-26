69d480e6ef972e0271e9161011efcb0f9f987c69
yusukemigitera <ymigitera@gmail.com>
2022-09-27 01:09:49 +0900

    node環境構築(js)

---
+++ node/README.md
@@ -0,0 +1,11 @@
+# baumkuchen/node
+
+## 動作確認
+
+```
+cd /node/src
+node main.js
+
+(別ターミナルで)
+curl http://localhost:8000 | jq
+```
---
+++ node/src/main.js
@@ -0,0 +1,23 @@
+import { createServer } from "http";
+import { parse } from "url";
+
+const port = 8000;
+
+const server = createServer((request, response) => {
+  const uri = parse(request.url || "").pathname;
+
+  response.writeHead(200, {
+    "Content-Type": "application/json",
+  });
+
+  if (uri === "/") {
+    response.end(
+      JSON.stringify({
+        message: "Hello",
+      })
+    );
+  }
+});
+
+server.listen(port);
+console.log(`The server has started and is listening on port number: ${port}`);
