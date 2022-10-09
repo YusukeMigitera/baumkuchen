0ca9b5458d04b33dbf9156f678a851c846114d19
yusukemigitera <ymigitera@gmail.com>
2022-10-09 12:25:50 +0900

    エラーパターン

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,5 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  useAddress,
+  useDisconnect,
+  useUser,
+  useLogin,
+  useLogout,
+  useMetamask,
+} from '@thirdweb-dev/react';
 
-const App = () => <>Hello</>;
+const App = () => {
+  const address = useAddress();
+  const connect = useMetamask();
+  const disconnect = useDisconnect();
+
+  const login = useLogin();
+  const logout = useLogout();
+  const { user } = useUser();
+
+  const [secret, setSecret] = useState();
+
+  const getSecret = async () => {
+    const res = await fetch('http:localhost:8000/secret');
+    const data = await res.json();
+    setSecret(data);
+  };
+
+  return (
+    <div>
+      {address ? (
+        <>
+          <button onClick={disconnect}>Disconnect Wallet</button>
+          <button onClick={() => login()}>Login with Wallet</button>
+          <button onClick={logout}>Logout</button>
+          <p>Your address: {address}</p>
+          <pre>User: {JSON.stringify(user || null)}</pre>
+          <br />
+          <br />
+          <button onClick={getSecret}>Get Secret</button>
+          <pre>Secret: {JSON.stringify(secret || null)}</pre>
+        </>
+      ) : (
+        <button onClick={connect}>Connect Wallet</button>
+      )}
+    </div>
+  );
+};
 
 export default App;
--- react/src/index.tsx
+++ react/src/index.tsx
@@ -3,15 +3,26 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
 
+// This is the chainId your dApp will work on.
+const activeChainId = ChainId.Mainnet;
 const container = document.getElementById('app');
 const root = createRoot(container!); // createRoot(container!) if you use TypeScript
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <ThirdwebProvider
+      desiredChainId={activeChainId}
+      authConfig={{
+        authUrl: '/api/auth',
+        domain: 'example.com',
+      }}
+    >
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </ThirdwebProvider>
   </React.StrictMode>
 );
